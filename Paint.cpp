d7f129eee2427434448904f4145c775b22b9eb53
lifantev <Lifantev.DA@phystech.edu>
2021-04-20 16:08:10 +0300

    Most functionality added. Need to fix filling operation.

---
+++ Paint.cpp
@@ -0,0 +1,162 @@
+#include "Paint.hpp"
+
+int main()
+{
+	txCreateWindow(1200, 600);
+
+	manager.Add(&windowForPainting);
+
+	manager.Add(new SetColorButton({ 555, 10 }, { 580, 35 }, TX_LIGHTBLUE));
+	manager.Add(new SetColorButton({ 580, 10 }, { 605, 35 }, TX_LIGHTRED));
+	manager.Add(new SetColorButton({ 605, 10 }, { 630, 35 }, TX_LIGHTGREEN));
+	manager.Add(new SetColorButton({ 630, 10 }, { 655, 35 }, TX_YELLOW));
+	manager.Add(new SetColorButton({ 655, 10 }, { 680, 35 }, TX_ORANGE));
+	manager.Add(new SetColorButton({ 680, 10 }, { 705, 35 }, TX_PINK));
+	manager.Add(new SetColorButton({ 705, 10 }, { 730, 35 }, TX_MAGENTA));
+
+	manager.Add(new FunctionalButton({ 0, 0 }, { 100, 50 }, "open", OpenImage));
+	manager.Add(new FunctionalButton({ 100, 0 }, { 200, 50 }, "save", SaveImage));
+	manager.Add(new FunctionalButton({ 100, 50 }, { 200, 100 }, "exit", ExitFromProgram));
+	manager.Add(new FunctionalButton({ 1020, 20 }, { 1120, 80 }, "clear", Clear));
+	manager.Add(new FunctionalButton({ 0, 50 }, { 50, 100 }, "<<", PrevState));
+	manager.Add(new FunctionalButton({ 50, 50 }, { 100, 100 }, ">>", NextState));
+
+	manager.Add(new InstrumentalButton({ 260, 10 }, { 290, 40 }, Pencil, "pencil.bmp"));
+	manager.Add(new InstrumentalButton({ 290, 10 }, { 320, 40 }, Spray, "spray.bmp"));
+	
+	// TODO: filling, now works with stack corruption most of the time
+	//manager.Add(new InstrumentalButton({ 320, 10 }, { 350, 40 }, CoverForFill, "filling.bmp"));
+
+	manager.Add(new ScrollBar({ 267, 60 }, { 507, 90 }, &radiusForPainting, 6));
+
+	manager.Add(new RGB_ScrollBar({ 790, 3 }, { 900, 32 }, &colorForPainting, RGB(255, 36, 0)));
+	manager.Add(new RGB_ScrollBar({ 790, 35 }, { 900, 64 }, &colorForPainting, RGB(102, 255, 0)));
+	manager.Add(new RGB_ScrollBar({ 790, 67 }, { 900, 96 }, &colorForPainting, RGB(125, 249, 255)));
+
+	windowForButtons.DrawWindow();
+
+	manager.DrawButtons();
+
+	manager.Run();
+}
+
+void Clear()
+{
+	txBitBlt(windowForPainting.canvas[windowForPainting.indexOfCanvas], 0, 0, COORD_RB_OF_MAIN_WINDOW.x - COORD_LT_OF_MAIN_WINDOW.x,
+		COORD_RB_OF_MAIN_WINDOW.y - COORD_LT_OF_MAIN_WINDOW.y, windowForPainting.canvas[0]);
+	txBitBlt(COORD_LT_OF_MAIN_WINDOW.x, COORD_LT_OF_MAIN_WINDOW.y, windowForPainting.canvas[0]);
+}
+
+void ExitFromProgram()
+{
+	windowForPainting.~Canvas();
+	exit(EXIT_SUCCESS);
+}
+
+void Pencil()
+{
+	Point coords1 = { txMouseX(), txMouseY() };
+	while (txMouseButtons() == LEFT_MOUSE_BUTTON)
+	{
+		Point coords2 = { txMouseX(), txMouseY() };
+		txSetColor(colorForPainting, radiusForPainting, windowForPainting.ReturnLastCanvas());
+		txLine(coords1.x, coords1.y - COORD_LT_OF_MAIN_WINDOW.y, coords2.x, coords2.y - COORD_LT_OF_MAIN_WINDOW.y, windowForPainting.ReturnLastCanvas());
+		txBitBlt(COORD_LT_OF_MAIN_WINDOW.x, COORD_LT_OF_MAIN_WINDOW.y, windowForPainting.ReturnLastCanvas());
+		txSleep();
+		coords1 = coords2;
+	}
+}
+
+void Spray()
+{
+	srand(time(0));
+	int random = rand();
+	while (txMouseButtons() == LEFT_MOUSE_BUTTON)
+	{
+		Point coords = { txMouseX(), txMouseY() };
+		for (int x = -radiusForPainting / 2; x <= radiusForPainting / 2; x++)
+		{
+			for (int y = -radiusForPainting / 2; y <= radiusForPainting / 2; y++)
+			{
+				if ((sqrt(x * x + y * y) <= radiusForPainting / 2) && (coords.y - y > COORD_LT_OF_MAIN_WINDOW.y))
+				{
+					srand(random);
+					random = rand();
+					if (random % 2 == 1)
+					{
+						txSetPixel(coords.x - x, coords.y - y, colorForPainting);
+						txSetPixel(coords.x - x, coords.y - y - COORD_LT_OF_MAIN_WINDOW.y, colorForPainting, windowForPainting.ReturnLastCanvas());
+					}
+					int time = 0;
+					while (time < 7000)
+						time++;
+				}
+				if (txMouseButtons() == 0)
+					break;
+			}
+		}
+		txSleep(1);
+	}
+}
+
+void SaveImage()
+{
+	txSaveImage("image.bmp");
+}
+
+void OpenImage()
+{
+	HDC sourceImage = txLoadImage("image.bmp");
+	txTransparentBlt(0, 0, sourceImage);
+	txDeleteDC(sourceImage);
+}
+
+void Fill(double x, double y, COLORREF current)
+{
+	HDC canvas = windowForPainting.ReturnLastCanvas();
+	if (current == txGetPixel(x, y) &&
+		y > windowForPainting.coordLT.y &&
+		y < windowForPainting.coordRB.y &&
+		x > windowForPainting.coordLT.x &&
+		x < windowForPainting.coordRB.x)
+	{
+		for (int i = -1; i <= 1; ++i)
+		{
+			for (int j = -1; j <= 1; ++j)
+			{
+				Fill(x + i, y + j, current);
+				txSetPixel(x, y - COORD_LT_OF_MAIN_WINDOW.y, colorForPainting, canvas);
+				txSetPixel(x, y, colorForPainting);
+			}
+		}
+	}
+	else
+	{
+		return;
+	}
+}
+
+void CoverForFill()
+{
+	Point coords = { txMouseX(), txMouseY() };
+	COLORREF current = txGetPixel(coords.x, coords.y);
+	Fill(coords.x, coords.y, current);
+}
+
+void PrevState()
+{
+	if (windowForPainting.canvas[windowForPainting.indexOfCanvas + 1] == NULL)
+		windowForPainting.indexOfCanvas -= 2;
+	else
+		windowForPainting.indexOfCanvas -= 1;
+	txBitBlt(COORD_LT_OF_MAIN_WINDOW.x, COORD_LT_OF_MAIN_WINDOW.y, windowForPainting.ReturnCurrentCanvas());
+}
+
+void NextState()
+{
+	if (windowForPainting.canvas[windowForPainting.indexOfCanvas + 1] != NULL)
+		windowForPainting.indexOfCanvas += 1;
+	txBitBlt(COORD_LT_OF_MAIN_WINDOW.x, COORD_LT_OF_MAIN_WINDOW.y, windowForPainting.ReturnCurrentCanvas());
+	if (windowForPainting.canvas[windowForPainting.indexOfCanvas + 1] == NULL)
+		windowForPainting.indexOfCanvas -= 1;
+}
--- Paint.hpp
+++ Paint.hpp
@@ -17,19 +17,31 @@ struct Point
 	double x, y;
 };
 
-void pencil();
-void fill(double x, double y, COLORREF current);
-void cover_for_fill();
+void Pencil();
+void Fill(double x, double y, COLORREF current);
+void CoverForFill();
+void Spray();
+void Clear();
+void ExitFromProgram();
+void SaveImage();
+void OpenImage();
+void PrevState();
+void NextState();
 
-func_t funcOfDrawing = &pencil;
+func_t FuncOfDrawing = &Pencil;
 
 COLORREF colorForPainting = TX_BLACK;
 bool isParametrsChanged = true;
 double radiusForPainting = 24;
-const Point coordLT_OfBut = { 0, 0 };
-const Point coordRB_OfBut = { 1200, 100 };
-const Point coordLT_OfMainWindow = { 0, 100 };
-const Point coordRB_OfMainWindow = { 1200, 600 };
+const Point COORD_LT_OF_BUT = { 0, 0 };
+const Point COORD_RB_OF_BUT = { 1200, 100 };
+const Point COORD_LT_OF_MAIN_WINDOW = { 0, 100 };
+const Point COORD_RB_OF_MAIN_WINDOW = { 1200, 600 };
+const int LEFT_MOUSE_BUTTON = 1;
+const int NUM_OF_CANVAS = 5000;
+const int NUM_OF_BUTTONS = 50;
+HDC virtualCanvas = txCreateCompatibleDC(COORD_RB_OF_MAIN_WINDOW.x - COORD_LT_OF_MAIN_WINDOW.x, COORD_RB_OF_MAIN_WINDOW.y - COORD_LT_OF_MAIN_WINDOW.y);
+
 
 class Window
 {
@@ -78,9 +90,7 @@ public:
 
 	virtual bool IfButtonPressed()
 	{
-		const int leftMouseButton = 1;
-
-		if (txMouseButtons() == leftMouseButton &&
+		if (txMouseButtons() == LEFT_MOUSE_BUTTON &&
 			txMouseX() >= coordLT.x &&
 			txMouseX() <= coordRB.x &&
 			txMouseY() >= coordLT.y &&
@@ -213,3 +223,251 @@ public:
 	{
 	}
 };
+
+class FunctionalButton : public BasicButton
+{
+public:
+	const char* name;
+	func_t function;
+
+	virtual void DrawButton() override
+	{
+		txSetColor(TX_LIGHTGRAY);
+		txSetFillColor(TX_WHITE);
+		txRectangle(coordLT.x, coordLT.y, coordRB.x, coordRB.y);
+		txDrawText(coordLT.x, coordLT.y, coordRB.x, coordRB.y, name);
+	}
+
+	virtual bool IfButtonPressed() override
+	{
+		if (txMouseButtons() == LEFT_MOUSE_BUTTON &&
+			txMouseX() >= coordLT.x &&
+			txMouseX() <= coordRB.x &&
+			txMouseY() >= coordLT.y &&
+			txMouseY() <= coordRB.y)
+		{
+			while (txMouseButtons() == LEFT_MOUSE_BUTTON)
+			{
+				txSleep(0.1);
+			}
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+
+	virtual void Action() override
+	{
+		function();
+	}
+
+	FunctionalButton(Point coordLT_, Point coordRB_, const char* name_, func_t function_) : BasicButton(coordLT_, coordRB_),
+		function(function_),
+		name(name_)
+	{
+	}
+};
+
+class InstrumentalButton : public BasicButton
+{
+public:
+	const char* sourceImage;
+	func_t function;
+
+	virtual void DrawButton() override
+	{
+		txSetColor(TX_LIGHTGRAY, 3);
+		txSetFillColor(TX_TRANSPARENT);
+		HDC sourceImageHDC = txLoadImage(sourceImage);
+
+		txTransparentBlt(coordLT.x, coordRB.y, sourceImageHDC);
+		txDeleteDC(sourceImageHDC);
+		txRectangle(coordLT.x, coordLT.y, coordRB.x, coordRB.y);
+	}
+
+	void RedrawButton1()
+	{
+		txSetColor(TX_GRAY, 5);
+		txSetFillColor(TX_TRANSPARENT);
+		txRectangle(coordLT.x, coordLT.y, coordRB.x, coordRB.y);
+	}
+
+	void RedrawButton2()
+	{
+		txSetColor(TX_WHITE, 5);
+		txSetFillColor(TX_TRANSPARENT);
+		txSleep();
+
+		txRectangle(coordLT.x, coordLT.y, coordRB.x, coordRB.y);
+		txSetColor(TX_LIGHTGRAY, 3);
+		txRectangle(coordLT.x, coordLT.y, coordRB.x, coordRB.y);
+	}
+
+	virtual void Action() override
+	{
+		RedrawButton1();
+		FuncOfDrawing = function;
+		RedrawButton2();
+	}
+
+	InstrumentalButton(Point coordLT_, Point coordRB_, func_t function_, const char* sourceImage_) : BasicButton(coordLT_, coordRB_),
+		function(function_),
+		sourceImage(sourceImage_)
+	{
+	}
+};
+
+class Canvas : public BasicButton
+{
+public:
+	HDC canvas[NUM_OF_CANVAS] = {};
+	func_t function;
+	int indexOfCanvas;
+
+	void Add(HDC canvas_)
+	{
+		int i;
+		for (i = 0; canvas[i] != NULL; ++i)
+		{
+		}
+		indexOfCanvas = i - 1;
+		canvas[i] = txCreateCompatibleDC(COORD_RB_OF_MAIN_WINDOW.x - COORD_LT_OF_MAIN_WINDOW.x,
+			COORD_RB_OF_MAIN_WINDOW.y - COORD_LT_OF_MAIN_WINDOW.y);
+		txBitBlt(canvas[i], 0, 0, COORD_RB_OF_MAIN_WINDOW.x - COORD_LT_OF_MAIN_WINDOW.x,
+			COORD_RB_OF_MAIN_WINDOW.y - COORD_LT_OF_MAIN_WINDOW.y, canvas_);
+	}
+
+	virtual void DrawButton() override
+	{
+		txSetColor(TX_LIGHTGRAY, 2, virtualCanvas);
+		txSetFillColor(TX_WHITE, virtualCanvas);
+		txRectangle(coordLT.x, coordLT.y - COORD_LT_OF_MAIN_WINDOW.y, coordRB.x, coordRB.y - COORD_LT_OF_MAIN_WINDOW.y, virtualCanvas);
+		txBitBlt(COORD_LT_OF_MAIN_WINDOW.x, COORD_LT_OF_MAIN_WINDOW.y, virtualCanvas);
+		Add(virtualCanvas);
+		Add(virtualCanvas);
+	}
+
+	virtual void Action() override
+	{
+		virtualCanvas = txCreateCompatibleDC(COORD_RB_OF_MAIN_WINDOW.x - COORD_LT_OF_MAIN_WINDOW.x, COORD_RB_OF_MAIN_WINDOW.y - COORD_LT_OF_MAIN_WINDOW.y);
+		int i;
+		for (i = indexOfCanvas + 2; canvas[i] != NULL; ++i)
+		{
+			canvas[i] = NULL;
+		}
+		txBitBlt(canvas[indexOfCanvas + 1], 0, 0, COORD_RB_OF_MAIN_WINDOW.x - COORD_LT_OF_MAIN_WINDOW.x,
+			COORD_RB_OF_MAIN_WINDOW.y - COORD_LT_OF_MAIN_WINDOW.y, canvas[indexOfCanvas]);
+		function();
+		txBitBlt(virtualCanvas, 0, 0, COORD_RB_OF_MAIN_WINDOW.x - COORD_LT_OF_MAIN_WINDOW.x,
+			COORD_RB_OF_MAIN_WINDOW.y - COORD_LT_OF_MAIN_WINDOW.y, ReturnLastCanvas());
+		Add(virtualCanvas);
+	}
+
+	HDC ReturnLastCanvas()
+	{
+		int i;
+		for (i = 0; canvas[i] != NULL; ++i)
+		{
+		}
+		indexOfCanvas = i - 1;
+		return canvas[i - 1];
+	}
+
+	HDC ReturnCurrentCanvas()
+	{
+		if (indexOfCanvas <= 0)
+		{
+			indexOfCanvas = 0;
+			return canvas[0];
+		}
+		else if (canvas[indexOfCanvas] == NULL)
+		{
+			indexOfCanvas = indexOfCanvas - 1;
+			return ReturnLastCanvas();
+		}
+		else
+		{
+			return canvas[indexOfCanvas];
+		}
+	}
+
+	Canvas(Point coordLT, Point coordRB, func_t function_, int indexOfCanvas_ = 0) : BasicButton(coordLT, coordRB),
+		function(function_), indexOfCanvas(indexOfCanvas_)
+	{
+	}
+
+	~Canvas()
+	{
+		for (int i = 0; canvas[i] != NULL; ++i)
+		{
+			txDeleteDC(canvas[i]);
+		}
+		txDeleteDC(virtualCanvas);
+	}
+};
+
+Canvas windowForPainting({ 0, 100 }, { 1200, 600 }, FuncOfDrawing);
+Window windowForButtons({ 0, 0 }, { 1200, 100 });
+ColorWindow indicator({ 910, 3 }, { 1003, 96 });
+
+class ManagerButton
+{
+public:
+	BasicButton* buttons[NUM_OF_BUTTONS];
+	void Add(BasicButton* button)
+	{
+		int i;
+		for (i = 0; buttons[i] != NULL; ++i)
+		{
+		}
+		buttons[i] = button;
+	}
+
+	void DrawButtons()
+	{
+		for (int i = 0; buttons[i] != NULL; i++)
+		{
+			(*buttons[i]).DrawButton();
+		}
+	}
+
+	void Run()
+	{
+		while (true)
+		{
+			for (int i = 0; buttons[i] != NULL; i++)
+			{
+				if ((*buttons[i]).IfButtonPressed())
+				{
+					(*buttons[i]).Action();
+				}
+			}
+			windowForPainting.function = FuncOfDrawing;
+			if (isParametrsChanged == true)
+			{
+				indicator.DrawWindow();
+				isParametrsChanged = false;
+			}
+		}
+	}
+
+	ManagerButton()
+	{
+		for (int i = 0; i < NUM_OF_BUTTONS; i++)
+		{
+			buttons[i] = NULL;
+		}
+	}
+
+	~ManagerButton()
+	{
+		for (int i = 0; buttons[i] != NULL; i++)
+		{
+			delete buttons[i];
+		}
+	}
+};
+
+ManagerButton manager;
